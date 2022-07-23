a3f7dd665b204377efad8c197026820d09e6bb8a
jawaher-alqotym <jwahralqtym@gmail.com>
2022-07-23 22:17:32 +0300

    add urls, serializers files

--- backend/mood_changer/mood_changer/settings.py
+++ backend/mood_changer/mood_changer/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user_app',
+    'mood_app',
+    'profile_app'
 ]
 
 MIDDLEWARE = [
