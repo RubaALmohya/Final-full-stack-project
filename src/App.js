ff97706a7dadd98ada84d5e318779518e15fd5b5
NouraSaad19 <noura.saad1419@gmail.com>
2022-07-23 18:38:55 +0300

    create project in VS

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+function App() {
+  return (
+    <div >
+      <Router>
+        <Routes>
+          <Route> </Route>
+        </Routes>
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
