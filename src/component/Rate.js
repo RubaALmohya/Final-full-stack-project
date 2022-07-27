a251a77e97ad2ede211c13972aff3453ee8c3ace
Hawra Alomani <ha0073@mix.wvu.edu>
2022-07-27 18:35:08 +0300

    Add rating works functionality

--- src/component/Rate.js
+++ src/component/Rate.js
@@ -1,9 +1,25 @@
 import React from "react";
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
+import axios from "axios";
+
 
 function Rate() {
+  let token = JSON.parse(localStorage.getItem('user'));
   const [value, setValue] = React.useState();
+
+
+  const add_rating = () => {
+    const url =`http://127.0.0.1:8000/profile_app/add_rating/`;
+    axios.post(url,{'rating':value},
+    {headers:{"Authorization" : `Bearer ${token}`}})
+  .then((res)=>{
+  }).catch((err)=>{
+      console.log(err)
+  })
+  //  window.location.reload()
+  }
+
   return (
     <div>
       <Typography component="legend">Rate your experiance: </Typography>
@@ -14,6 +30,7 @@ function Rate() {
           setValue(newValue);
         }}
       />
+      <button onClick={add_rating}> submit </button>
     </div>
   );
 }
